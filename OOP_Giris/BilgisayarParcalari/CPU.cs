74d61cc08d323ce1411e1d0993eb85aafbec5298
Veysel ÖREN <orenveysel@outlook.com>
2024-04-22 14:53:43 +0300

    Add project files.

---
+++ OOP_Giris/BilgisayarParcalari/CPU.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Giris.BilgisayarParcalari
+{
+    public class CPU
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public int CoreSayisi { get; set; }
+        public float Fiyat { get; set; }
+
+
+    }
+}
---
+++ OOP_Giris/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace OOP_Giris
+{
+    /*
+    Bilgisayar parcalari
+    Anakart, Cpu, Ram, SSD, HDD, EkranKarti, NetworkKarti
+    Kasa, PowerSupplier, Ekran, Klavye, Mouse
+    Printer, Scanner, Hoparlor, WebCam
+
+
+    */
+    public class Ogrenci
+    {
+        public int Id; //Filled
+        public string Ad; //Filled
+        public string Soyad; //Filled
+        private string TcNo; //Filled
+        public DateTime DogumTarihi; //Filled
+
+        //Property => Ozellik
+        //Kontrollu veri alimi icin gerekli set metoduna sahiptir
+        public string TcKimlik {
+            get 
+            {
+                return TcNo.Substring(7);
+            }
+            set
+            {
+                if(string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Tc kimlik alani bos olamaz!");
+                    return;
+                }
+                foreach (Char ch in value)
+                {
+                    if (!Char.IsDigit(ch))
+                    {
+                        Console.WriteLine("Girilen ifade icerisinde sayisal olmayan degerler var");
+                        return;
+                    }
+                }
+                if (value[0].ToString() == "0")
+                {
+                    Console.WriteLine("Tc No 0 ile baslayamaz!");
+                }
+                if (value.Length == 11)
+                {
+                    TcNo = value;
+                }
+                else
+                {
+                    Console.WriteLine("Tc numarasi 11 Karakter olmalidir!");
+                    return;
+                }
+            }
+        } 
+
+        public string Gsm { get; set; } //Bos Prop => kontrolsuz
+        //Ogrenci Class'ina ait metodlar
+        public void SetTcNo(string tcNo)
+        {
+            if(tcNo.Length != 11 )
+            {
+                Console.WriteLine("Tc numarasi 11 Karakter olmalidir!");
+            }
+            else if (tcNo.Length == 11)
+            {
+                TcNo = tcNo;
+            }
+        }
+
+        public string GetTcNo()
+        {
+            //Tc No'nun son 4 hanesi geri donecek
+            return TcNo.Substring(7);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Ogrenci ogrenci = new Ogrenci();
+            ogrenci.Id = 1;
+            ogrenci.Ad = "Ali";
+            ogrenci.Soyad = "Yilmaz";
+            ogrenci.SetTcNo("12345678901");
+            ogrenci.TcKimlik = "sadasgasgas";
+            Console.WriteLine(ogrenci.TcKimlik);
+        }
+    }
+}
