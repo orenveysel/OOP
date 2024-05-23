28ad2a01f6aba8fa27d37bfa36336fdc6b02b5c8
Veysel ÖREN <orenveysel@outlook.com>
2024-05-24 00:57:29 +0300

    Delegate Sorgular Lab

---
+++ 23-DelegateSorgularLab/Personel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Bogus.DataSets.Name;
+
+namespace _23_DelegateSorgularLab
+{
+    public  class Personel
+    {
+        public string FullName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+        public string Email { get; set; }
+        public string Gsm { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ 23-DelegateSorgularLab/Program.cs
@@ -0,0 +1,72 @@
+﻿using Bogus;
+namespace _23_DelegateSorgularLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             *  Listelerde kullanilan pek cok metod ActionDelegate ile
+             *  Func delegate alir. Eger Func delegate'in donus tipi bool ise
+             *  buna predicate delegate denir.
+             * 
+             * 
+             */
+
+            Faker faker = new Faker("tr");
+            List<Person> liste = new List<Person>()
+            {
+                new Person(locale:"tr"),
+                new Person(locale:"tr"),
+                new Person(locale:"tr"),
+                new Person(locale:"tr"),
+                new Person(locale:"tr"),
+                new Person(locale:"tr"),
+                new Person(locale:"tr"),
+                new Person(locale:"tr"),
+                new Person(locale:"tr"),
+                new Person(locale:"tr"),
+                new Person(locale:"tr")
+            };
+            List<Person> liste2 = new();
+            for (int i = 0; i < 100; i++)
+            {
+                liste2.Add(new Person(locale: "tr"));
+            }
+
+            var femaleList = liste2.Where(p=>p.Gender.ToString()=="Female").ToList();
+            //Metodlu kullanimi
+            var femaleList2 = liste2.Where(CinsiyetBul).ToList();
+
+            // femaleList.ForEach(p => Console.WriteLine(p.FirstName + " " + p.LastName + " " + p.Address.City));
+            Console.WriteLine("**************************");
+            var maleList = liste2.Where(p => p.Gender.ToString() == "Male").ToList();
+            //maleList.ForEach(p => Console.WriteLine(p.FirstName + " " + p.LastName + " " + p.Address.City));
+
+            // Burasi Ercan'i bulamadigi zaman hata verecektir
+            //var ilkErkekEleman = maleList.First(p=>p.FirstName=="Ercan");
+            var ilkKadinEleman = femaleList.First();
+
+            // Burada FirstOrDefault ömetodu herhangi bir sonuc bulamaz ise hata vermez
+            // Geriye null doner
+            var ilkErkekEleman2 = maleList.FirstOrDefault(p => p.FirstName == "Ercan");
+            var ilkKadinEleman2 = femaleList.FirstOrDefault();
+
+
+            Console.WriteLine(maleList.Contains(ilkErkekEleman2));
+
+            Person ayse = new Person() { Gender = Bogus.DataSets.Name.Gender.Female, FirstName = "Ayse", LastName = "Kaya" };
+            var sonuc = femaleList.FirstOrDefault(ayse);
+            femaleList.Add(ayse);
+            sonuc = femaleList.FirstOrDefault(ayse);
+
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static bool CinsiyetBul(Person person)
+        {
+            return person.Gender.ToString() == "Male";
+        }
+    }
+}
